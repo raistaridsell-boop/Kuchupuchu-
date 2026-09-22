f777a433c90975e3f54c951ebeb86bb4a0295be6
raistaridsell-boop <raistaridsell@gmail.com>
2026-09-22 21:13:39 +0530

    Modify API URL and key for Shruti integration
    Updated API endpoint and key variables for Shruti API.
    
    Signed-off-by: raistaridsell-boop <raistaridsell@gmail.com>

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # Vars For API End Pont.
-YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
-YT_API_KEY = getenv("YT_API_KEY" , None ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
+API_URL = os.environ.get("SHRUTI_API_URL", "https://api.shrutibots.site") 
+API_KEY = os.environ.get("SHRUTI_API_KEY", "YOUR_API_KEY") ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
 
 ## Other vaes
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
